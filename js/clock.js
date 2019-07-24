0d46801494e6e290179de8c71b2c7afda4d8de6e
narahan <narahan1006@gmail.com>
2019-07-24 19:44:12 +0900

    화면에 시간 노출

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@
 
 
 const clockContainer = document.querySelector('.js-clock'),
-	clockTitle = clockContainer.querySelector('h1');
+	clockText = clockContainer.querySelector('h3');
 
 function getTime(){
 	//시, 분, 초
@@ -11,11 +11,11 @@ function getTime(){
 		min = date.getMinutes(),
 		sec = date.getSeconds();
  		
- 		console.log('date--->', hour, min, sec < 10);
+ 		// console.log('date--->', hour, min, sec < 10);
 
 
 		// 한 자리수 경우 앞에 0을 붙임
-		clockTitle.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${min < 10 ? `0${min}` : min} : ${sec < 10 ? `0${sec}` : sec}`;
+		clockText.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${min < 10 ? `0${min}` : min} : ${sec < 10 ? `0${sec}` : sec}`;
 }
 
 
